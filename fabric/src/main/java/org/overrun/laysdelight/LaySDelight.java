ada64229eb0e282938938cf68cee2d45046c3f83
baka4n <qq474899581@gmail.com>
2023-07-06 14:00:47 +0800

    添加基础设置

---
+++ fabric/src/main/java/org/overrun/laysdelight/LaySDelight.java
@@ -0,0 +1,11 @@
+package org.overrun.laysdelight;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LaySDelight implements ModInitializer {
+    public static final String MOD_ID = "laysdelight";
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ forge/src/main/java/org/overrun/laysdelight/LaySDelight.java
@@ -0,0 +1,13 @@
+package org.overrun.laysdelight;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static org.overrun.laysdelight.LaySDelight.MOD_ID;
+
+@Mod(MOD_ID)
+public class LaySDelight {
+    public static final String MOD_ID = "laysdelight";
+    public LaySDelight() {
+
+    }
+}
